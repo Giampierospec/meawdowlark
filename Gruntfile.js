06bce62cff410b5793d157017be877ddaa842514
Giampiero Specogna <giampi_12@hotmail.com>
2017-05-16 17:10:27 -0400

    QA

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt){
+    //load plugins
+    [
+        "grunt-cafe-mocha",
+        "grunt-contrib-jshint",
+        "grunt-exec"
+    ].forEach(function(task){
+        grunt.loadNpmTasks(task);
+    });
+
+    //configure plugins
+    grunt.initConfig({
+        cafemocha: {
+            all:{src:"qa/test-*.js", options: {ui: "tdd"}}
+        },
+        jshint: {
+            app:["meawdowlark.js", "public/js/**/*.js","lib/**/*.js"],
+            qa:["Gruntfile.js", "public/qa/**/*.js", "qa/**/*.js"]
+        }
+    });
+    // register tasks
+    grunt.registerTask("default", ["cafemocha","jshint"]);
+};
