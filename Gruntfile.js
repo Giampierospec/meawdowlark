00d50a0bddd169430c6707566596a3d1e2028755
Giampiero Specogna <giampi_12@hotmail.com>
2017-05-23 09:32:28 -0400

    changed mostly everything

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,28 @@
 module.exports = function(grunt){
-    //load plugins
-    [
-        "grunt-cafe-mocha",
-        "grunt-contrib-jshint",
-        "grunt-exec"
-    ].forEach(function(task){
-        grunt.loadNpmTasks(task);
-    });
 
-    //configure plugins
-    grunt.initConfig({
-        cafemocha: {
-            all:{src:"qa/test-*.js", options: {ui: "tdd"}}
-        },
-        jshint: {
-            app:["meawdowlark.js", "public/js/**/*.js","lib/**/*.js"],
-            qa:["Gruntfile.js", "public/qa/**/*.js", "qa/**/*.js"]
-        }
-    });
-    // register tasks
-    grunt.registerTask("default", ["cafemocha","jshint"]);
+	// load plugins
+	[
+		'grunt-cafe-mocha',
+		'grunt-contrib-jshint',
+		'grunt-exec',
+	].forEach(function(task){
+		grunt.loadNpmTasks(task);
+	});
+
+	// configure plugins
+	grunt.initConfig({
+		cafemocha: {
+			all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
+		},
+		jshint: {
+			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+		},
+		exec: {
+			linkchecker: { cmd: 'linkchecker --ignore-url=\'!^(https?:)\/\/localhost\b\' http://localhost:3000' }
+		},
+	});	
+
+	// register tasks
+	grunt.registerTask('default', ['cafemocha','jshint','exec']);
 };
